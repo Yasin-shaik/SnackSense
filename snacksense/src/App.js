64ab8593880916dff86bbe62cfc5f9665a0aa325
Yasin-shaik <skyasin2004@gmail.com>
2025-01-27 19:48:55 +0530

    Login and Register Forms Done

--- snacksense/src/App.js
+++ snacksense/src/App.js
@@ -1,9 +1,20 @@
 import './App.css';
-
+import Landing from './Pages/Landing';
+import Home from './Pages/Home';
+import Register from './Pages/Register';
+import Login from './Pages/Login';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 function App() {
   return (
     <>
-      Hlo, this is the first react app
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<Landing/>}/>
+          <Route exact path="/home" element={<Home/>}/>
+          <Route exact path="/register" element={<Register/>}/>
+          <Route exact path="/login" element={<Login/>}/>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
